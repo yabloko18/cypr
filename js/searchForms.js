e1f9251411bfb051902f602f315f741f2de6e64c
yabloko18 <73183432+yabloko18@users.noreply.github.com>
2022-08-22 04:15:49 +0400

    upd filter

--- js/searchForms.js
+++ js/searchForms.js
@@ -3,20 +3,33 @@ const forms = document.querySelectorAll('form.search-form');
 if (forms.length > 0) {
     forms.forEach(function (e) {
         let categories = e.querySelectorAll('input[name=category]');
-        let category = null;
-/*
-        let prices = e.querySelectorAll('')
-*/
-        categories.forEach(function (e) {
-            if (e.checked ) {
-                category = e;
-            }
-            e.addEventListener('click', function () {
-                if (e.checked ) {
-                    category = e;
+        let activeCategory = null;
+        let categoryBlocks = e.querySelectorAll('[data-category]');
+
+        if (categories.length > 0) {
+            categories.forEach(function (e) {
+                if (e.checked) {
+                    activeCategory = e.value
+                    setCategoryBlocks(activeCategory)
                 }
-                console.log(category.attributes)
+                e.addEventListener('click', function () {
+                    if (e.checked) {
+                        activeCategory = e.value
+                        setCategoryBlocks(activeCategory)
+                    }
+                })
             })
-        })
+        }
+
+        function setCategoryBlocks(category) {
+            let activeCategoryBlocks = e.querySelectorAll(`[data-category=${category}]`);
+            categoryBlocks.forEach(function (e) {
+                e.parentElement.classList.add('d-none')
+            })
+            activeCategoryBlocks.forEach(function (e) {
+                e.parentElement.classList.remove('d-none')
+                e.parentElement.classList.add('d-flex')
+            })
+        }
     })
 }
